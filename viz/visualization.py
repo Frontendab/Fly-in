47aef0161238029c3f2359f55a55bb4f76d46e91
Frontendab <ayoubpourletravail@gmail.com>
2026-02-23 17:27:05 +0000

    Set image as background and start drawing

--- viz/visualization.py
+++ viz/visualization.py
@@ -1,11 +1,25 @@
 import pygame
+from enum import Enum
+
+
+class AttributeWindow(Enum):
+    X = 3500
+    Y = 1700
+    image_path = "assets/bg.png"
 
 
 def start_visualization():
     pygame.init()
 
-    pygame.display.set_mode((500, 500))
     pygame.display.set_caption("Fly-in")
+
+    screen = pygame.display.set_mode(
+        (AttributeWindow.X.value, AttributeWindow.Y.value)
+    )
+    bg_image = pygame.image.load(AttributeWindow.image_path.value)
+    bg_image = pygame.transform.scale(
+        bg_image, (AttributeWindow.X.value, AttributeWindow.Y.value)
+    )
 
     running = True
     while running:
@@ -13,5 +27,8 @@ def start_visualization():
             if event.type == pygame.QUIT:
                 running = False
 
-    # * Pygame quit
+        screen.blit(bg_image, (0, 0))
+        pygame.display.update()
+
+    # * pygame quit
     pygame.quit()
